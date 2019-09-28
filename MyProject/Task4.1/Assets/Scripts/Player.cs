9370ca700bac17d433a979916bd6f4c884dc5fff
AnastasiaZ4 <ninelia1793@gmail.com>
2019-09-28 16:02:33 +0300

    Добавлены сцены и возможность перемещения между ними

---
+++ MyProject/Task4.1/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static Player _instance;
+    private void Start()
+    {
+        _instance = this;
+    }
+
+    public static Vector3 GetPosition()
+    {
+        return _instance.transform.position;
+    }
+
+    public static void SetPosition(Vector3 _position)
+    {
+        _instance.transform.position = new Vector3(_position.x, 0.0f, _position.z);
+    }
+
+}
+
---
+++ MyProject/Task4.1/Assets/Scripts/Restart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    /// <summary>
+    /// id - сцены.
+    /// </summary>
+    private int _idScene;
+
+    /// <summary>
+    /// Название сцены.
+    /// </summary>
+    public string _nextSceneName;
+    //public string _nextSceneName = "Level2";
+
+
+    /// <summary>
+    /// Загружает следующий уровень.
+    /// </summary>
+    public void UploadtScene()
+    {
+        // int _idNextScene = _idScene + 1;
+        // SceneManager.LoadScene(_idNextScene);      
+        SceneManager.LoadScene(_nextSceneName);
+    }
+
+    /// <summary>
+    /// Перезапускает текущий уровень.
+    /// </summary>
+    public void ReloadLevel()
+    {
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        _idScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(_idScene);
+    }
+}
